c7884e3d3231b8ceae7066f3d3c5088a85d1b648
chuanchuan <62045791+89461561511656@users.noreply.github.com>
2021-07-07 19:10:49 +0800

    Add files via upload

---
+++ opencv测试.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	//读取图片，这里修改为你的图片路径，注意需要将单斜杠写成双斜杠
+	Mat img = imread("C://Users//hp//Pictures//Camera Roll//QQ图片20210429234831.jpg");
+	imshow("IMG", img); //显示图片
+	waitKey(0);
+	return 0;
+}
